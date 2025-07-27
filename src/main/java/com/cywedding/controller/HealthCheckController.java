4fdacdfb30ccd6946c3100b38bf89a689a74596d
송찬영 <payo03@naver.com>
2025-07-27 15:30:35 +0900

    07-27 git ignore추가, NeonDB Wake를 위한 Dummy Query호출 추가

--- src/main/java/com/cywedding/controller/HealthCheckController.java
+++ src/main/java/com/cywedding/controller/HealthCheckController.java
@@ -3,14 +3,33 @@ package com.cywedding.controller;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cywedding.dto.QRUser;
+import com.cywedding.mapper.QRUserMapper;
 
 @RestController
 public class HealthCheckController {
 
+    @Autowired
+    private QRUserMapper userMapper;
+
     @GetMapping("/healthcheck")
-    public String healthCheck() {
-        return "SERVER IS RUNNING : " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    public String healthCheck(@RequestParam(required = false) String code) {
+
+        String result = "SERVER IS RUNNING : " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        if (code != null) {
+            QRUser user = userMapper.fetchQRUser(code);
+            if (user != null) {
+                result += "\nUSER: " + user;
+            } else {
+                result += "\nUSER: not found for code = " + code;
+            }
+        }
+
+        return result;
     }
 }
